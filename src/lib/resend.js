6d2506d01ff7400349a839e84e1ced90749ed3b9
mohammedsancreator-art <mohammedsan.creator@gmail.com>
2026-02-23 10:49:58 +0100

    adding backend

---
+++ src/lib/resend.js
@@ -0,0 +1,12 @@
+// src/lib/resend.js
+import { Resend } from "resend";
+import dotenv from "dotenv";
+
+dotenv.config(); // باش يقرا ملف .env
+
+export const resendClient = new Resend(process.env.RESEND_API_KEY);
+
+export const sender = {
+  email: process.env.EMAIL_FROM,
+  name: process.env.EMAIL_FROM_NAME,
+};
