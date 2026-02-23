1c942e789539f246a3b0b4aa3dc8adc496219253
mohammedsancreator-art <mohammedsan.creator@gmail.com>
2026-02-23 11:47:59 +0100

    Update cookie settings for JWT

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -11,13 +11,12 @@ export const generateToken = (userId, res) => {
     expiresIn: "7d",
   });
 
-  res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
-    secure: ENV.NODE_ENV === "development" ? false : true,
-  });
-
+res.cookie("jwt", token, {
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: "none",
+  secure: ENV.NODE_ENV !== "development",
+});
   return token;
 };
 
