1d59cdb0e1f1dfd436030502ab638e26d0f015c2
mohammedsancreator-art <mohammedsan.creator@gmail.com>
2026-02-24 10:26:58 +0100

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -15,7 +15,7 @@ res.cookie("jwt", token, {
   maxAge: 7 * 24 * 60 * 60 * 1000,
   httpOnly: true,
   sameSite: "none",
-  secure: ENV.NODE_ENV !== "development",
+  secure: true,
 });
   return token;
 };
